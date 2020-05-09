bdafdaf9cade5b3422f4029303aac6557ebc78f3
andrewallred <andrewrallred@gmail.com>
2020-05-09 18:07:51 -0400

    .

--- features/slack_features.js
+++ features/slack_features.js
@@ -33,6 +33,7 @@ module.exports = function(controller) {
 
             console.log('got an object');
             console.log(selectedObject);
+            console.log(message);
 
             bot.replyPublic(message, selectedObject);
 
