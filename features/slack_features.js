8a14cd2e51473525971eee9e9fb5db589b9fa7cc
andrewallred <andrewrallred@gmail.com>
2020-06-14 12:39:45 -0400

    removing some logs

--- features/slack_features.js
+++ features/slack_features.js
@@ -78,8 +78,8 @@ module.exports = function(controller) {
 
             const selectData = JSON.parse(message.text.replace('shuffle ', ''));
 
-            console.log("selectData is ");
-            console.log(selectData);
+            //console.log("selectData is ");
+            //console.log(selectData);
 
             let searchResult;
             if (selectData.attempt % 3 == 0) {
