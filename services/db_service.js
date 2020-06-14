44ac08df9276021a98ae073bfdd42d63571894ca
andrewallred <andrewrallred@gmail.com>
2020-06-14 12:52:55 -0400

    .

--- services/db_service.js
+++ services/db_service.js
@@ -97,7 +97,7 @@ async function getObjectForSearchTerm(searchTerm) {
     let skipCount = Math.floor(Math.random() * collectionCount);
     console.log('skip count ' + skipCount);
     
-    let temp = collection.findOne(query);
+    let temp = await collection.findOne(query);
     //let temp = await collection.find(query).limit(1).skip(skipCount);
 
     //console.log(temp);
